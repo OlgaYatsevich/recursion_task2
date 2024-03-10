79c061e5d659df82616544b5911965352490859c
Olga Yatsevich <olevichya@gmail.com>
2024-03-10 19:49:26 +0300

    добавили решение задачи2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    NaturalNumber(num - 1);
+    Console.Write($"{num} ");
+}
+NaturalNumber(number);
